c3e43f2ab6af12f780afd6bacbe360b9eb0f17ad
Tarlison Sander <tarlison_brito@hotmail.com>
2018-10-09 18:11:07 -0400

    Codificação

--- Índice Invertido/main.c
+++ Índice Invertido/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lista.h"
 int main(){
     FILE *arquivo;
-    arquivo = open("teste.txt","r");
+    int c,auxC = 0;
+    arquivo = fopen("teste.txt.txt","r");
     if (arquivo == NULL){
         printf("Houve um erro ao tentar abrir o arquivo.\n");
     }
+    while((c = fgetc(arquivo)) != EOF){
+        printf("Caractere lido: %c\n", c);
+        if(c != ' '){
+            auxC++;
+        }
+    }
+    printf("QUANTIDADE DE CARACTERES: %d", auxC);
 
+
+    if((c == EOF) && (feof(arquivo) == 0) && (ferror(arquivo) != 0))
+        perror("Erro: fgetc");
+
+
+    fclose(arquivo);
 
 
 }
