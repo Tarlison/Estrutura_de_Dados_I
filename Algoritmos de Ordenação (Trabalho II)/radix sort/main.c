4fb72adf43519cb537e54b9e70dba8c3a4a0687f
Luigi Muller <sluigimuller@gmail.com>
2018-10-20 16:34:18 -0400

    Trabalho escrito e radix sort

---
+++ Algoritmos de Ordenação (Trabalho II)/radix sort/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void radixsort(int vetor[], int tamanho) {
+    int i, *b, exp = 1;
+    int maior = vetor[0];
+    b = (int *)calloc(tamanho, sizeof(int));
+    for (i = 0; i < tamanho; i++) {
+        if (vetor[i] > maior)
+    	    maior = vetor[i];
+    }
+    while (maior/exp > 0) {
+        int bucket[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    	for (i = 0; i < tamanho; i++)
+    	    bucket[(vetor[i] / exp) % 10]++;/*guarda a quantidade de vezes que o dígito de uma ordem especifica
+    	    (unidade, dezena, etc)*/
+    	for (i = 1; i < 10; i++)
+    	    bucket[i] += bucket[i - 1];/*ele faz uma coisa doida aqui*/
+    	for (i = tamanho - 1; i >= 0; i--)
+    	    b[--bucket[(vetor[i] / exp) % 10]] = vetor[i];
+    	for (i = 0; i < tamanho; i++)
+    	    vetor[i] = b[i];
+    	exp *= 10;
+    }
+    free(b);
+}
+
+
+int main(){
+    int cont, tamanho;
+    srand(time(NULL));
+    printf("Hello world!\n");
+    return 0;
+}
